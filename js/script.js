d68f3e0d40bface10ecc4113d84e23b5493f93f7
leovianna <leonardovianna95@hotmail.com>
2016-03-19 18:55:23 +0000

    atelração

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-/**
- * Created by LUCAS on 18/03/2016.
- */
 $( document ).ready(function() {
-    //Ativa o menu no mobile
-    $(".button-collapse").sideNav();
-    var telaAlt = $( window ).height();
-    // Aplica o tamanho da foto de acordo com a tela
-    $('.home').css('height',telaAlt);
-
-    /*var menuAlt = $('.menuSite').height();
-
-    var contentHomeAlt = telaAlt - menuAlt;
-    $('.contentHome').css('height',contentHomeAlt);*/
+  var telaAlt = $(window).height();
+  var telaHome = telaAlt-64;
+  $('.home').css("height",telaHome);
 });
